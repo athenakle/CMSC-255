612e98175cec2c9b6a94a0ab89ba11bf7cf43aa1
Athena Le <athenale12@gmail.com>
2023-03-01 20:57:42 -0500

    Create NameArt.java

---
+++ NameArt.java
@@ -0,0 +1,29 @@
+
+/*
+NameArt
+
+Project 1 – Name in Code
+This projects outputs my name in a unique way
+	
+Athena Le
+January,25,2023
+CMSC 255-901
+*/
+
+public class NameArt{
+	public static void main(String[] args){
+		System.out.println("Athena Le");
+		System.out.println("Project 1-Name in Code");
+		System.out.println("This project outputs my name in a unique way on the display screen");
+		System.out.println("January 25, 2023");
+		System.out.println("CMSC 255-901");
+		System.out.println("          ______        _____                               ");
+		System.out.println("     //\\   ||  ||   || |    *     *    //\\  ");
+		System.out.println("    //  \\  ||  ||   || |    * *   *   //  \\ ");
+		System.out.println("   //====\\ ||  ||===|| |=== *  *  *  //====\\  ");
+		System.out.println("  //      \\||  ||   || |    *   * * //      \\ ");
+		System.out.println(" //        \\|  ||   || |____*    **//        \\  ");
+	}   
+	}
+
+
