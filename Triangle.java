7541571d9dde6b0dab3aab8a6564a6fadfacadb7
Athena Le <athenale12@gmail.com>
2023-03-01 20:55:52 -0500

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,28 @@
+
+/*
+*Triangle java
+*
+*Athena Le
+*1/25/23
+*/
+
+
+public class Triangle
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("      // \\");
+		System.out.println("     //   \\");
+		System.out.println("    //     \\");
+		System.out.println("   //  ^_^  \\");
+		System.out.println("  //         \\");
+		System.out.println(" //           \\");
+		System.out.println("//             \\");
+		System.out.println("_________________");
+
+	}
+}
+
+
+
